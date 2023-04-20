356a0f47f50ab36b0a961c0d66453b307fef84c7
Philip Mathieu <philip.eng.mathieu@gmail.com>
2023-04-19 20:13:16 -0400

    Drop last batch if smaller than batch size

--- src/train.py
+++ src/train.py
@@ -60,7 +60,7 @@ def train_model(
 
     # 3. Create data loaders
     loader_args = dict(batch_size = batch_size, num_workers=os.cpu_count(), pin_memory=True)
-    train_loader = DataLoader(train_set, shuffle=True, **loader_args)
+    train_loader = DataLoader(train_set, shuffle=True, drop_last=True, **loader_args)
     val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
     # (Initialize logging)
